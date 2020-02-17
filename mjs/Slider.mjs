84ad18e10ada02ef923009138a94be5fbfc81f2b
denis <denisstathatos@hotmail.com>
2020-02-17 14:38:48 -0500

    fin

--- mjs/Slider.mjs
+++ mjs/Slider.mjs
@@ -15,23 +15,24 @@ export class Slider {
         let elmSlides = document.createElement('div')
         elmSlides.classList.add('slides')
         let elmNav = document.createElement('nav')
-        elmNav.classList.add('nav')
         let k=1
         
         for (let unSlide of this.list_slide){
             let elmSlide = document.createElement('div')
             let elmBtn = document.createElement('a')
-            elmSlide.id = 'slides-' + k
-            elmBtn.href = '#slide-' + k
+            elmSlide.id = 'slides-' + k 
+            elmBtn.href = '#slides-' + k
             let elmImg = document.createElement("img")  
             elmImg.src = unSlide.img
             elmSlide.appendChild(elmImg)
             elmSlides.appendChild(elmSlide)
             elmNav.appendChild(elmBtn)
+            k++
         }
 
         
         elmSlider.appendChild(elmSlides)
+        elmSlider.appendChild(elmNav)
         this.elmMonSlider.appendChild(elmSlider)
 
 
